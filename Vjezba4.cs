2fbb3b65af07ea48a36645f0294a8f232631c4e5
ValentinoVidec <valentinovidec1505@gmail.com>
2024-12-09 13:47:02 +0100

    Add files via upload

---
+++ Vjezba4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba4
+{
+    internal class Program
+    {
+        class KlasaX
+        {
+            int broj;
+
+            
+            private int Broj { get => broj; set => broj = value; }
+
+            public KlasaX() { }
+
+            public KlasaX(int broj)
+            {
+                this.broj = broj;
+            }
+
+            public override string ToString()
+            {
+                string ispis = "Vrijednost broja je " + this.broj;
+                return ispis;
+            }
+        }
+        static void Main(string[] args)
+        {
+            KlasaX KlasaX = new KlasaX(10);
+            Console.WriteLine(KlasaX.ToString());
+            Console.ReadKey();
+        }
+    }
+}
